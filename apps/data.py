3dc7b39bd1e2e564d817e93ec3b706c5ab601fcf
marioruizgonzalez <marioruizgonzalez.mx@gmail.com>
2022-10-19 13:10:59 -0500

    mrg - clean base

--- apps/data.py
+++ apps/data.py
@@ -1,7 +1,4 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-from sklearn import datasets
 
 def app():
     st.title('Data')
@@ -10,10 +7,3 @@ def app():
 
     st.write("The following is the DataFrame of the `iris` dataset.")
 
-    iris = datasets.load_iris()
-    X = pd.DataFrame(iris.data, columns = iris.feature_names)
-    Y = pd.Series(iris.target, name = 'class')
-    df = pd.concat([X,Y], axis=1)
-    df['class'] = df['class'].map({0:"setosa", 1:"versicolor", 2:"virginica"})
-
-    st.write(df)
